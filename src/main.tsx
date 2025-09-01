f60867c2c7c6f1d94f70f7d03e5cf6b5c7d6be7a
Yeras <erasylserikov36@gmail.com>
2025-09-01 12:40:05 +0500

    v1.0001

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import Layout from "./components/Layout.tsx";
 
 createRoot(document.getElementById('root')!).render(
